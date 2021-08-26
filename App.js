6918c612e3b0fece4732a9edc0d8a92544fbc18b
ChinonsoIg <ojononso@yahoo.com>
2021-08-26 03:24:06 +0100

    fixed inProgress dispatch

--- App.js
+++ App.js
@@ -19,15 +19,7 @@ import { store } from "./redux/store";
 
 
 import firebase from "firebase/app";
-const firebaseConfig = {
-  apiKey: "AIzaSyCzj_doR5odD6sdXnvJL2ncpwiIL-HkVhU",
-  authDomain: "react-native-f2f5f.firebaseapp.com",
-  databaseURL: "https://react-native-f2f5f-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "react-native-f2f5f",
-  storageBucket: "react-native-f2f5f.appspot.com",
-  messagingSenderId: "609734983271",
-  appId: "1:609734983271:web:cbe5230643d3d0f91cdec2"
-};
+
 
 // This checks that no other instance of firebase is running
 if (firebase.apps.length === 0) {
